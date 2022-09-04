abaf180990bbb89c60799e2ab9ebf46127ccad9d
Junhyeok Heo <wnsgur5508@gmail.com>
2022-09-04 21:57:58 +0900

    address api framework

--- member/js/join_own_2.js
+++
@@ -1,8 +0,0 @@
-const $id = document.querySelector('.id');
-const $check = document.querySelector('.fa-check');
-
-function express_id_check() {
-    if($id.value == "") {
-        $check.style.color='green';
-    }
-};
