5501dca469c509ded2be67150b09e1b1a28bbf6c
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-09-05 16:46:41 +0900

    fix position & try

--- member/js/info.js
+++ member/js/info.js
@@ -4,6 +4,3 @@ function show() {
 function close() {
     document.querySelector(".background").className = "background";
 }
-
-document.querySelector(".confirm-btn").addEventListener('click', show);
-document.querySelector(".confirm-btn").addEventListener('click', close);
